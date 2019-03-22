8f69238d3d4109ca930b843be1207795cf96464c
andreahowes <ashcreek.howes@gmail.com>
2019-03-21 21:54:07 -0400

    insert weather function added

--- src/main/java/mybatis/mappers/WeatherMapper.java
+++ src/main/java/mybatis/mappers/WeatherMapper.java
@@ -8,7 +8,13 @@ import org.apache.ibatis.annotations.Select;
 public interface WeatherMapper {
 
     String GET_BY_CITY = "SELECT * FROM `mybatis-test`.Weather where city = #{city}";
+    String INSERT_WEATHER = "INSERT INTO `mybatis-test`.Weather (city, raining, temperatureInF) " +
+            "VALUES (#{city}, #{raining}, #{temperatureInF})";
 
     @Select(GET_BY_CITY)
     public Weather getByCity(String city);
+
+    @Select(INSERT_WEATHER)
+    public Weather insertWeather(Weather weather);
+
 }
